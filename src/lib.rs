44cd2893a56c1fb9e9731e1ad0952973a1357270
miocryp <mio.cryp@gmail.com>
2023-05-20 12:06:49 +0200

    Initial

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+use cosmwasm_std::{entry_point, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult};
+#[entry_point]
+pub fn instantiate(
+    _deps: DepsMut, 
+    _env: Env, 
+    _info: MessageInfo, 
+    _msg: Empty,
+) -> StdResult<Response>  {
+    Ok(Response::new())
+}
+
+#[entry_point]
+pub fn execute(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: Empty,
+) -> StdResult<Response> {
+    Ok(Response::new())
+}
+
+#[entry_point]
+pub fn query(
+    _deps: Deps,
+    _env: Env,
+    _msg: Empty,
+) -> StdResult<Binary> {
+    Ok(Binary::default())
+}
+
